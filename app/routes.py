44643660f275092e82f1c7f5c07b077c9dab4acd
Jelte Fennema <github-tech@jeltef.nl>
2016-04-28 20:32:37 +0200

    Return 502 HTTP error code on failed attempt

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 """Routes of the web application."""
 
-from flask import render_template, url_for, redirect, request, flash
+from flask import render_template, request, flash
 
 from app.mail import RedundantMail
 from app import app
@@ -13,8 +13,8 @@ def home():
     return render_template('home.jade')
 
 
-@app.route('/mail', methods=['POST'])
-def send_email():
+@app.route('/', methods=['POST'])
+def send_mail():
     """Route to send actual email."""
 
     used_form_vals = {k: request.form[k] for k in
@@ -24,8 +24,10 @@ def send_email():
         provider = RedundantMail(**used_form_vals).send()
     except RuntimeError as e:
         flash(e.args[0], 'danger')
+        return_code = 502
     else:
         flash("Email was successfully sent using " + provider.capitalize(),
               'success')
+        return_code = 200
 
-    return redirect(url_for('home'))
+    return home(), return_code
